9b08ed0f98fa462cc0fbc518a8ce9c3dd4f5aa9e
Jussi Vaihia <jussi.vaihia@futurice.com>
2014-10-08 15:51:30 +0300

    cli/bash mixup fix

--- maybe.bash
+++ maybe.bash
@@ -10,9 +10,11 @@ echo $CMD
 $CMD
 
 echo -e "\nNo ImportError because we add current path to sys.path using PYTHONPATH"
-CMD='PYTHONPATH= fab hello'
-echo $CMD
+CMD='fab hello'
+export PYTHONPATH=''
+echo "PYTHONPATH= fab hello"
 $CMD
+unset PYTHONPATH
 
 echo -e "\nNo ImportError, because directory *of script being run* is automatically added to sys.path"
 CMD='python hello.py'
@@ -20,7 +22,7 @@ echo $CMD
 $CMD
 
 echo -e "\nImportError follows during *task execution* because current directory is not in sys.path"
-echo "(import would work outside the function; affects dynamic imports)"
+echo "The import would work outside the function. Only affects runtime imports"
 CMD='fab hello'
 echo $CMD
 $CMD
