49203896caa802e1fb02a3b87116e8547f84f3c1
Jussi Vaihia <jussi.vaihia@futurice.com>
2014-10-08 15:44:37 +0300

    README and docs on examples

--- README.md
+++ README.md
@@ -1,4 +1,3 @@
-fabpath
-=======
+Testing import handling in Python projects with an example using Fabric. Aims to shed light on expectations on what should and should not work.
 
-bash maybe.bash
+$ bash maybe.bash
--- maybe.bash
+++ maybe.bash
@@ -4,12 +4,23 @@ source venv/bin/activate
 pip install -r requirements.txt
 unset PYTHONPATH
 
-echo "No ImportError due 'python -m' adding the current directory (='') to sys.path:"
-python -m fabric hello
+echo -e "\nNo ImportError due 'python -m' adding the current directory (='') to sys.path:"
+CMD='python -m fabric hello'
+echo $CMD
+$CMD
 
-echo "No ImportError because we add current path to sys.path using PYTHONPATH"
-PYTHONPATH= fab hello
+echo -e "\nNo ImportError because we add current path to sys.path using PYTHONPATH"
+CMD='PYTHONPATH= fab hello'
+echo $CMD
+$CMD
 
-echo "ImportError follows during *task execution* because current directory is not in sys.path"
-echo "(import would work outside the function; the sys.path state affects runtime imports)"
-fab hello
+echo -e "\nNo ImportError, because directory *of script being run* is automatically added to sys.path"
+CMD='python hello.py'
+echo $CMD
+$CMD
+
+echo -e "\nImportError follows during *task execution* because current directory is not in sys.path"
+echo "(import would work outside the function; affects dynamic imports)"
+CMD='fab hello'
+echo $CMD
+$CMD
